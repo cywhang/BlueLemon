4f2725fec7a13a38e9d0cc4c1309c5959e229a46
pe052 <pe052@Hyeok>
2023-06-20 12:18:57 +0900

    devlop commit

--- src/main/java/com/blue/view/MainController.java
+++ src/main/java/com/blue/view/MainController.java
@@ -46,7 +46,7 @@ public class MainController {
 	private ReplyService replyService;
 	@Autowired
 	private FollowService followService;
-	
+	//
 	// 로그인 페이지로 이동
 	@GetMapping(value="/login")
 	public String login(){
