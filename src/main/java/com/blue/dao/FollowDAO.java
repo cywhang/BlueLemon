32b25cd7d30f10aba07deaaa61b63dac2dce1f77
pe052 <pe052@Hyeok>
2023-06-30 16:14:54 +0900

    ㅅㄳㄳ

--- src/main/java/com/blue/dao/FollowDAO.java
+++ src/main/java/com/blue/dao/FollowDAO.java
@@ -14,7 +14,6 @@ public class FollowDAO {
 	
 	@Autowired
 	private SqlSessionTemplate mybatis;
-	// ÆÈ·ÎÀ×
 	public List<FollowVO> getFollowing(String member_Id) {
 		return mybatis.selectList("FollowMapper.getFollowing", member_Id);
 	}
